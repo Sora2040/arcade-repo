de480d549aa2eb0719f0ea49fbc633f4698b0770
Isaiah Marris <i.marris@yahoo.com>
2023-02-14 19:54:35 -0600

    4th commit

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-let gameBoardTTT = [
-    [undefined, undefined, undefined],
-    [undefined, undefined, undefined],
-    [undefined, undefined, undefined]
-]
-for (let i = 0; i < gameBoardTTT.length; i++) {
-    let currentRow = gameBoardTTT[i]; 
-    let xTracker
-    let oTracker
-    for (let j = 0; j < gameBoardTTT.length; j++) {
-        if (currentRow[j] == "X") {
-            xTracker = xTracker + 1
-        } else {
-             oTracker = oTracker + 1
-        }
-    }
+let mainSectionEle = document.getElementById("main-section")
+
+let counterHeaderEle = document.getElementById("clicker-count")
+
+let gameBoard = [];
+
+let valueOfClickCount = 0;
+
+function increaseClickCount(event) {
+    
 }
